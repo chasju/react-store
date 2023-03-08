a00654700e6ab7ad36bbe18d6430461ca18fa160
chasju <91468816+chasju@users.noreply.github.com>
2023-03-08 12:37:58 +0100

    Adds search

---
+++ store/src/Components/Search.jsx
@@ -0,0 +1,12 @@
+import styles from "../styles/Search.module.css";
+
+export default function Search() {
+  return (
+    <div>
+      <form action="submit">
+        <i className="fa-solid fa-magnifying-glass"></i>
+        <input type="text" name="searchInput" placeholder="Search" />
+      </form>
+    </div>
+  );
+}
