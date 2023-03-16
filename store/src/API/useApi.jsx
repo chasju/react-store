6ab7652beae7cacce7858d7654051ae5d0d4e1a7
chasju <91468816+chasju@users.noreply.github.com>
2023-03-16 13:06:35 +0100

    Adds loader and error message

--- store/src/API/useApi.jsx
+++ store/src/API/useApi.jsx
@@ -2,21 +2,31 @@ import { useState, useEffect } from "react";
 
 function useApi(url) {
   const [posts, setPosts] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
 
   useEffect(() => {
     async function getData() {
       try {
+        setIsLoading(true);
+
         const response = await fetch(url);
         const json = await response.json();
-        setPosts(json);
+
+        if (response.ok) {
+          setPosts(json);
+          setIsLoading(false);
+        }
+        setIsError(true);
       } catch (error) {
         console.log(error);
+        setIsError(true);
       }
     }
 
     getData();
   }, [url]);
-  return { posts };
+  return { posts, isLoading, isError };
 }
 
 export default useApi;
--- store/src/Components/Cards.jsx
+++ store/src/Components/Cards.jsx
@@ -1,10 +1,21 @@
 import styles from "../styles/Card.module.css";
+import errorStyles from "../styles/ErrorMessage.module.css";
+import loaderStyles from "../styles/Loader.module.css";
 import useApi from "../API/useApi";
 
 export default function Cards() {
-  const { posts } = useApi("https://api.noroff.dev/api/v1/online-shop");
+  const { posts, isLoading, isError } = useApi("https://api.noroff.dev/api/v1/online-shop");
 
-  console.log(posts);
+  if (isLoading) {
+    return <div className={loaderStyles.loader}></div>;
+  }
+
+  if (isError) {
+    <div className={errorStyles.errorContainer}>
+      <p className={errorStyles.errorMessage}>There was an error loading the content. Please contact us if the issue persists.</p>
+    </div>;
+  }
+
   return (
     <div className={styles.cardContainer}>
       {posts.map((post) => (
