e40ea4d73e29fd7596c2344fd8250900c92d62a5
chasju <91468816+chasju@users.noreply.github.com>
2023-03-30 17:55:46 +0200

    Adds details to cart

--- store/src/Components/CartItem.jsx
+++ store/src/Components/CartItem.jsx
@@ -3,12 +3,29 @@ import styles from "../styles/ProductList.module.css";
 
 export default function CartItem({ product, id, removeProduct }) {
   const [count, setCount] = useState(1);
+  const [productTotal, setProductTotal] = useState(product.discountedPrice);
+  const [oldProductTotal, setOldProductTotal] = useState(product.oldPrice);
+  const [isDisabled, setIsDisabled] = useState(true);
 
   function increment() {
     setCount(count + 1);
+    const total = productTotal + product.discountedPrice;
+    const oldPrice = oldProductTotal + product.oldPrice;
+
+    setProductTotal(parseFloat(total.toFixed(2)));
+    setOldProductTotal(parseFloat(oldPrice.toFixed(2)));
+
+    setIsDisabled(false);
   }
+
   function decrement() {
     setCount(count - 1);
+    const total = productTotal - product.discountedPrice;
+    const oldPrice = oldProductTotal - product.oldPrice;
+    setProductTotal(parseFloat(total.toFixed(2)));
+    setOldProductTotal(parseFloat(oldPrice.toFixed(2)));
+
+    count === 2 ? setIsDisabled(true) : setIsDisabled(false);
   }
 
   return (
@@ -21,18 +38,18 @@ export default function CartItem({ product, id, removeProduct }) {
           <div className={styles.titleContainer}>
             <h2 className={styles.title}>{product.title}</h2>
             <div className={styles.countContainer}>
-              <button className={styles.countButton} onClick={decrement}>
+              <button disabled={isDisabled} className={styles.countButton} onClick={decrement}>
                 <i className="fa-solid fa-minus"></i>
               </button>
-              <p className={styles.count}>{count < 0 ? setCount(0) : count}</p>
+              <p className={styles.count}>{count < 1 ? setCount(1) : count}</p>
               <button className={styles.countButton} onClick={increment}>
                 <i className="fa-solid fa-plus"></i>
               </button>
             </div>
           </div>
           <div className={styles.priceContainer}>
-            <p className={styles.oldPrice}>{product.oldPrice} NOK</p>
-            <p className={styles.newPrice}>{product.discountedPrice} NOK</p>
+            <p className={styles.oldPrice}>{product.oldPrice > product.discountedPrice ? oldProductTotal + " NOK" : ""}</p>
+            <p className={styles.newPrice}>{productTotal} NOK</p>
             <button
               className={styles.trash}
               onClick={() => {
