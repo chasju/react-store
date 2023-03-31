a70312f0dd4dd9fd77612532e0b10110cd796e7a
chasju <91468816+chasju@users.noreply.github.com>
2023-03-31 14:37:58 +0200

    Adds contact page

--- store/src/Pages/Contact.jsx
+++ store/src/Pages/Contact.jsx
@@ -1,3 +1,30 @@
+import styles from "../styles/Contact.module.css";
+
 export default function Contact() {
-  return <div>Contact form</div>;
+  return (
+    <div className={styles.contactContainer}>
+      <h1>Contact</h1>
+      <div>
+        <form className={styles.formContainer}>
+          <div>
+            <label htmlFor="">Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">Email</label>
+            <input type="email" />
+          </div>
+          <div>
+            <label htmlFor="">Subject</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">Your message here</label>
+            <textarea name="" id="" cols="30" rows="10"></textarea>
+          </div>
+          <button className={styles.contactButton}>Submit</button>
+        </form>
+      </div>
+    </div>
+  );
 }
